b14ec4a72a9c0e52fcb0b6ec6a203110f4c1989a
13631 <13631@DESKTOP-3M75H1Q>
2021-03-26 14:29:33 +0800

    项目建立+导入idea中编写的前端页面

---
+++ Sheji/src/main/java/com/hy/Sheji/web/HelloAction.java
@@ -0,0 +1,14 @@
+package com.hy.Sheji.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloAction {
+
+	  @GetMapping("index.do")
+	  public String hello() {
+		  return "index";
+	  }
+}
---
+++ Sheji/src/test/java/com/hy/Sheji/ShejiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hy.Sheji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShejiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
